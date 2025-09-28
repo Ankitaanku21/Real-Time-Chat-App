2cac5cd6f90bade8c946ee1131ed116c8fc3895d
ankitaanku21 <ankita.as.singh9@gmail.com>
2025-09-28 13:05:19 +0530

    change in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,12 +18,18 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+  "http://localhost:5173"
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
